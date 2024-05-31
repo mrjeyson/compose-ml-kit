10a6d3ae616b8e61608ccfafdae41442ce849573
mrjeyson <71706561+mrjeyson@users.noreply.github.com>
2024-05-31 11:23:09 +0500

    ...

--- app/src/main/java/com/jsoft/composemlkit/MainActivity.kt
+++ app/src/main/java/com/jsoft/composemlkit/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.jsoft.composemlkit.ui.theme.ComposeMLKitTheme
 
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
