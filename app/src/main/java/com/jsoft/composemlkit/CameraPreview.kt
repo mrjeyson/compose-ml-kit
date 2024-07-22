f65c118036c0debc475ac16e993d4a8c6a836ecd
mrjeyson <71706561+mrjeyson@users.noreply.github.com>
2024-07-22 09:22:06 +0500

    ...

--- app/src/main/java/com/jsoft/composemlkit/CameraPreview.kt
+++ app/src/main/java/com/jsoft/composemlkit/CameraPreview.kt
@@ -218,6 +218,7 @@ fun OverlayFrame() {
             size = Size(canvasWidth - rectLeft - rectWidth, rectHeight)
         )
 
+
         // Draw the frame
         drawRect(
             color = Color.White,
