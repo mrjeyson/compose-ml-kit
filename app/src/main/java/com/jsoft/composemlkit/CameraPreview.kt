ec03d3eed0a25b7f20d23dd2f23f4f5baf433d61
Jasurbek <jasurbekkurganbayev@gmail.com>
2025-06-24 11:52:12 +0900

    ...

--- app/src/main/java/com/jsoft/composemlkit/CameraPreview.kt
+++ app/src/main/java/com/jsoft/composemlkit/CameraPreview.kt
@@ -56,7 +56,6 @@ fun CameraPreview(
     modifier: Modifier, onTextRecognized: (String) -> Unit
 ) {
 
-
     val context = LocalContext.current
     val lifecycleOwner = context as LifecycleOwner
     val cameraProviderFuture = remember {
